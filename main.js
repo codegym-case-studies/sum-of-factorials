558f09d33df26941c31a7f920d708d2de3992ed0
= <ndangduc.bn@gmail.com>
2019-04-19 11:02:30 +0700

    "Add file nhom 2: Ham tinh tong"

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 alert('Sum of factories is' + sum(factoryAll(getNumbers())))
+function sum(factoryAll) {
+    let sum =0;
+    for (let i = 0; i < factoryAll.length; i++) {
+        sum += factoryAll[i];
+    }
+    document.write(sum);
+}
