0fcc04bc08d78b5d7168197baf7e6c6505cd04d5
wesleyHnguyen <hongthanhnguyen85@gmail.com>
2019-04-19 11:10:18 +0700

    factory

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 alert('Sum of factories is' + sum(factoryAll(getNumbers())))
+
+function factory(number) {
+    let factoryNum = 1;
+    if (number === 0 || number === 1) {
+        return factoryNum;
+    }
+    for (let i = 2; i <= number; i++) {
+        factoryNum *= i;
+    }
+    return factoryNum;
+}
